70c5956e7e12f4088e530fa4db0bbd4a73bda4b1
Thiago Neves <nevesluzthiago@gmail.com>
2024-09-20 13:52:43 -0300

    group objects and add player movement

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ def main():
     pygame.init()
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # objects groups
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
 
+    Player.containers = (drawable_group, updatable_group)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # delta time - time that has passed since the last frame was drawn
     deltaTime = 0
 
@@ -21,11 +26,13 @@ def main():
                 sys.exit()
             
         window.fill(BLACK)
-        player.update(deltaTime)
+        
+        for obj in updatable_group:
+            obj.update(deltaTime)
+        for obj in drawable_group:
+            obj.draw(window)
 
-        player.draw(window)
         pygame.display.flip()
-
         # limit framerate
         deltaTime = clock.tick(FRAMERATE) / 1000
 
--- player.py
+++ player.py
@@ -15,18 +15,26 @@ class Player(CircleShape):
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = self.position + forward * self.radius # type: ignore
+        b = self.position - forward * self.radius - right # type: ignore
+        c = self.position - forward * self.radius + right # type: ignore
         return [a, b, c]
-    
+
     def update(self, deltaTime):
         keys = pygame.key.get_pressed()
         
-        if keys[pygame.K_a]:
-            self.rotate(-deltaTime)
         if keys[pygame.K_d]:
             self.rotate(deltaTime)
+        if keys[pygame.K_a]:
+            self.rotate(-deltaTime)
+        if keys[pygame.K_w]:
+            self.move(deltaTime)
+        if keys[pygame.K_s]:
+            self.move(-deltaTime)
+
+    def move(self, deltaTime):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * deltaTime
 
     def rotate(self, deltaTime):
         self.rotation += PLAYER_TURN_SPEED * deltaTime
